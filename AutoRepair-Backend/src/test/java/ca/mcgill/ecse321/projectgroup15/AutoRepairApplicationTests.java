439dd7f6cafbe3a8dc24c7f79619a8d02bbfae26
raniaouassif <rania.ouassif@mail.mcgill.ca>
2021-02-18 00:37:53 +0100

    Initial commit of the backend application

---
+++ AutoRepair-Backend/src/test/java/ca/mcgill/ecse321/projectgroup15/AutoRepairApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.projectgroup15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRepairApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
