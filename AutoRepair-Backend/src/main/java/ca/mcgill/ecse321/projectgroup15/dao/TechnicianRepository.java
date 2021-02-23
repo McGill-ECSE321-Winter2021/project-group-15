3138ce316f0f989a07c26a612fc99a7571fed106
raniaouassif <rania.ouassif@mail.mcgill.ca>
2021-02-23 01:55:55 +0100

    fixed typo of CRUD TechnicianRepository

--- AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/projectgroup15/dao/TechnicianRepository.java
+++ AutoRepair-Backend/src/main/java/ca/mcgill/ecse321/projectgroup15/dao/TechnicianRepository.java
@@ -7,12 +7,11 @@ import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.projectgroup15.model.Technician;
 
 public interface TechnicianRepository extends CrudRepository<Technician, String>{
-<<<<<<< Updated upstream
 
 	Technician findTechnicianByTechnicianID(String technicianID);
-=======
+
 	
 	Technician findTechnicianByUserID(String UserID);
->>>>>>> Stashed changes
+
 
 }
