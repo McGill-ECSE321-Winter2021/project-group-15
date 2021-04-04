cb6955e4eae252f21694835859d08ddb3c2cce97
elieruban <46905487+elieruban@users.noreply.github.com>
2021-04-03 20:44:01 -0400

    update

--- AutoRepair-Frontend/src/components/RegistrationPerson.js
+++ AutoRepair-Frontend/src/components/RegistrationPerson.js
@@ -8,14 +8,28 @@ function CustomerDto (email, username, password, lastName, firstName){
 
 }
 
+function TechnianDto (email, username, password, lastName, firstName){
+    this.email=email
+    this.username=username
+    this.password=password
+    this.lastName=lastName
+    this.firstName=firstName
+}
+
+
+
 
 
 export default {
     name: 'createAccount',
     data(){
         return{
-
+            
         }
     }
 
+}
+
+methods: {
+   
 }
