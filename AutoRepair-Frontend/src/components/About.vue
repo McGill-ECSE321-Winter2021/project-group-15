788b626028ad6f99da79a49081dba173fe939adf
elieruban <46905487+elieruban@users.noreply.github.com>
2021-04-03 22:18:31 -0400

    About us vue

--- AutoRepair-Frontend/src/components/About.vue
+++ AutoRepair-Frontend/src/components/About.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="About">
-<div class="title">
-<h2>OPENING HOURS</h2>
-</div<
+    <div class="title">
+    <h2>OPENING HOURS</h2>
+  </div<
 <table>
   <tr>
     <th>Monday</th>
@@ -31,9 +31,31 @@
     <td> <time>10:00</time>am - <time>1:00</time> pm</td>
   </tr>
 </table>
+<p> </p>
+<p> </p>
+
+<p> At Auto Repair Shop we always use the latest diagnostic and specialty tools
+    to provide a fast, precise, efficient service and repair for our loyal client’s
+    vehicles. From traditional maintenance and service to performance modifications
+    and motorsports prep, our techs have the knowledge and attention to detail to keep
+    your car performing optimally. </p>
+
+  <p> </p>
+
+<p> The trust and loyalty of our repeat customers is what drives our success and through
+    decades of experience we have developed a reputation for our knowledge and expertise.
+    We measure our performance by the comments and feedback of our loyal customers always
+    providing high quality work and exceptional service.</p>  
+<p> </p>
+<p> Email: autorepairshop@gmail.com </p>
+
+<p> Phone number: 514-123-4567</p>
+
+<p> Address: 6955 St Hubert St, Montreal, Quebec H2S 2N1 </p>
 
 
 </div>
+  
 </div>
 </template>
 <script>
@@ -43,19 +65,22 @@
 .title {
   background-color: black;
   color: white;
-padding: 0px 40px;
-  text-align: left;
+  padding: 0px 40px;
+  text-align: center;
   font-size: 12px;
   float: center;
 
 }
 table {
+  position: relative;
    border-collapse: collapse;
   width: 100%;
   border: 1px solid #ddd;
+  margin:auto;
 }
 
 th, td {
+
 	width: 50%;
   text-align: center;
   padding: 16px;
@@ -66,8 +91,16 @@ tr:nth-child(even) {
   background-color: #f2f2f2;
 }
 
+p {
+  font-family:Sans-Serif; 
+  text-align: center; 
+  font-size: 20px}
+ #grey {background-color:grey;}
 
 </style>
+
+
+
 
 
 
